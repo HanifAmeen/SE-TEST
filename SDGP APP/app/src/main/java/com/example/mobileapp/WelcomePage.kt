2e1f9e24c451b853aa0ffdfc67d15ede2da3c541
JaniduChathumina <86459858+JaniduChathumina@users.noreply.github.com>
2022-02-09 01:19:56 +0530

    SDGP APP files

---
+++ SDGP APP/app/src/main/java/com/example/mobileapp/WelcomePage.kt
@@ -0,0 +1,11 @@
+package com.example.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_welcome)
+    }
+}
